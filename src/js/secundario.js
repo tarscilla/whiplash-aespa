170fa9220fa47091c2f49b396ac35b2a8b6dd1ab
tarscilla <almeidatarscilla@hotmail.com>
2025-02-08 18:43:44 -0300

    initial commit

---
+++ src/js/secundario.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let audio = document.getElementById("bg-musica");
+
+    if (!audio) {
+        return;
+    }
+
+    audio.volume = 0.5;
+    audio.play();
+});
+
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+btnAvancar.addEventListener("click", function () {
+    if(cartaoAtual === cartoes.length - 1) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);
+});
+
+btnVoltar.addEventListener("click", function() {
+    if(cartaoAtual === 0) return;
+
+    esconderCartaoSelecionado();
+
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
+});
+
+function mostrarCartao(cartaoAtual) {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
